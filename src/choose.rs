e36b531376a5d72d00464e9bb10c30183cf3028d
aragami3070 <smirnov17612@gmail.com>
2025-07-29 02:07:54 +0400

    Chore: remove useless clone

--- src/choose.rs
+++ src/choose.rs
@@ -6,8 +6,7 @@ pub fn random_wallpaper(
     mut wallpapers: Vec<Wallpaper>,
     active_wallpaper: ActiveWallpaper,
 ) -> Result<NewWallpaper, Box<dyn Error>> {
-    let remove_active_w = active_wallpaper.0.clone();
-    wallpapers.retain(|x| (*x).path != remove_active_w.path);
+    wallpapers.retain(|x| (*x).path != active_wallpaper.0.path);
 
     let rand_num = rand::random_range(0..wallpapers.len() - 1);
 
