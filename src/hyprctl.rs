77f32a79807272047c6b8deab1bbd446a61411ba
aragami3070 <smirnov17612@gmail.com>
2025-07-28 21:00:30 +0400

    Fix: remove useless suberror and rename ErrorVariant to HyprctlErrorKind

--- src/hyprctl.rs
+++ src/hyprctl.rs
@@ -7,44 +7,26 @@ pub struct Wallpaper {
 pub struct ActiveWallpaper(pub Wallpaper);
 
 #[derive(Debug)]
-pub struct HyprpaperError {
-    description: String,
-}
-
-impl fmt::Display for HyprpaperError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.description)
-    }
-}
-
-impl Error for HyprpaperError {}
-
-#[derive(Debug)]
-enum ErrorVariant {
-    ListActive(HyprpaperError),
+enum HyprctlErrorKind {
+    ListActive,
 }
 
 #[derive(Debug)]
 pub struct HyprctlError {
-    variant: ErrorVariant,
+    kind: HyprctlErrorKind,
+    description: String,
 }
 
-impl Error for HyprctlError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match &self.variant {
-            ErrorVariant::ListActive(err) => Some(err),
-        }
-    }
-}
+impl Error for HyprctlError {}
 
 impl fmt::Display for HyprctlError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.variant {
-            ErrorVariant::ListActive(err) => {
+        match &self.kind {
+            HyprctlErrorKind::ListActive => {
                 write!(
                     f,
                     "run hyprctl hyprpaper listactive error received.\nDescription: {}",
-                    err.description
+                    &self.description
                 )
             }
         }
@@ -53,14 +35,13 @@ impl fmt::Display for HyprctlError {
 
 pub fn get_active_wallpaper() -> Result<ActiveWallpaper, Box<dyn Error>> {
     let list_active = Command::new("hyprctl")
-        .args(&["hyprpaper", "listactive"])
+        .args(["hyprpaper", "listactive"])
         .output()?;
 
     if !list_active.status.success() {
         return Err(Box::new(HyprctlError {
-            variant: ErrorVariant::ListActive(HyprpaperError {
-                description: String::from_utf8(list_active.stderr)?,
-            }),
+            kind: HyprctlErrorKind::ListActive,
+            description: String::from_utf8(list_active.stderr)?,
         }));
     }
 
