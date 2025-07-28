f1b518b4fbfac9965d3fe6a1a28208a7f3d80d22
aragami3070 <smirnov17612@gmail.com>
2025-07-29 01:24:51 +0400

    Feat: add match args command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+mod choose;
+mod dir_scan;
+mod hyprctl;
+
+use clap::Parser;
 use std::process;
 
-mod hyprctl;
-mod dir_scan;
+use crate::dir_scan::{Args, CliCommand, get_all_wallpapers};
 
 fn main() {
+    let args: Args = Args::parse();
     let active_wallpaper = match hyprctl::get_active_wallpaper() {
         Ok(w) => w,
         Err(err) => {
@@ -13,4 +18,36 @@ fn main() {
     };
 
     println!("Result: {active_wallpaper:?}");
+
+    match args.command {
+        CliCommand::Rand { dir_path } => {
+            let wallpapers = match get_all_wallpapers(dir_path) {
+                Ok(w) => w,
+                Err(err) => {
+                    eprintln!("Error: {err}");
+                    process::exit(1);
+                }
+            };
+
+            for wallpaper in wallpapers {
+                println!("{wallpaper:?}")
+            }
+        }
+
+        CliCommand::Next { dir_path } => {
+            let mut wallpapers = match get_all_wallpapers(dir_path) {
+                Ok(w) => w,
+                Err(err) => {
+                    eprintln!("Error: {err}");
+                    process::exit(1);
+                }
+            };
+
+            wallpapers.sort_by_key(|a| a.path.0.clone());
+
+            for wallpaper in wallpapers {
+                println!("{wallpaper:?}")
+            }
+        }
+    }
 }
