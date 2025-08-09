26130beb3405560cd3009273c60914992c3993ba
aragami3070 <smirnov17612@gmail.com>
2025-08-09 23:20:01 +0400

    Feat: add handler for prev command

--- src/args_handler.rs
+++ src/args_handler.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 
 use crate::{
-    choose::{next_wallpaper, random_wallpaper},
-    dir_scan::{Args, CliCommand, get_all_wallpapers},
-    hyprctl::{ActiveWallpaper, set_new_wallpaper},
+    choose::{next_wallpaper, prev_wallpaper, random_wallpaper},
+    dir_scan::{get_all_wallpapers, Args, CliCommand},
+    hyprctl::{set_new_wallpaper, ActiveWallpaper},
 };
 
 pub fn handler(args: Args, active_wallpaper: ActiveWallpaper) -> Result<(), Box<dyn Error>> {
@@ -25,6 +25,19 @@ pub fn handler(args: Args, active_wallpaper: ActiveWallpaper) -> Result<(), Box<
             wallpapers.sort_by_key(|a| a.path.0.clone());
 
             let new_wallpaper = next_wallpaper(wallpapers, active_wallpaper)?;
+
+            match set_new_wallpaper(new_wallpaper) {
+                Ok(_) => Ok(()),
+                Err(err) => Err(err),
+            }
+        }
+
+        CliCommand::Prev { dir_path } => {
+            let mut wallpapers = get_all_wallpapers(dir_path)?;
+
+            wallpapers.sort_by_key(|a| a.path.0.clone());
+
+            let new_wallpaper = prev_wallpaper(wallpapers, active_wallpaper)?;
 
             match set_new_wallpaper(new_wallpaper) {
                 Ok(_) => Ok(()),
